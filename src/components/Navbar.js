710d1e4ff6bc5b554f1da710536826ce6b047854
Sumit Singh <sumitsingh@Sumits-MacBook-Air.local>
2022-02-07 09:54:45 +0530

    buttons and text summary

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,27 +5,25 @@ import PropTypes from 'prop-types';
 export default function Navbar(props) {
   return <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <a className="navbar-brand" href="/">SumitDemo</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <a className="navbar-brand" href="/">{props.title}</a>
-
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Home</a>
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">{props.aboutText}</a>
+              <a className="nav-link" href="/">Resume</a>
             </li>
-            {/* <li className="nav-item">
-              <a className="nav-link disabled" href="/">Disabled</a>
-            </li> */}
+            <li className="nav-item">
+              <a className="nav-link" href="/">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="/">Contact</a>
+            </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
         </div>
       </nav>
   </div>;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,15 +4,20 @@ export default function TextForm(props) {
   const [text, setText]=useState("Enter text here");
   const handleUpClick = () => {
     console.log("uppercase was clicked for: ",text);
-    setText("you have clicked on button");
+    // text="i don't want to be contacted, please dont email me";
+    setText(text.toUpperCase());
+    // setText("you have clicked on button");
   }
   const handleOnChange = (event)=>{
     console.log("on change");
     setText( event.target.value );
   }
+  const handleClear = ()=>{
+    setText("");
+  }
   return <div>
     <hr/>
-      
+      <div className="container">
         <h1>{props.heading}</h1>
         <div className="form-group">
           <label for="exampleFormControlInput1">Email address</label>
@@ -23,8 +28,15 @@ export default function TextForm(props) {
           <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleOnChange} ></textarea>
         </div><br/>
         {/* <button className="btn btn-primary">Convert to uppercase</button> */}
-        <button className="btn btn-primary" onClick={handleUpClick} >Send email to me</button>
-      
+        <button className="btn btn-primary mx-2" onClick={handleUpClick} >Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2"  >Send email to me</button>
+        <button className="btn btn-primary mx-2" onClick={handleClear} >Clear</button>
+      </div>
+
+      <div className="container2 my-3">
+        <h6>Your text summary</h6>
+        <p>{text.split(" ").length} words and {text.length} characters. Time to read for me: {text.split(" ").length*0.008} minutes</p>
+      </div>
 
   </div>;
 }
